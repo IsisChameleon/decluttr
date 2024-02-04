6e42476edcab1ce4e1ec0898658823c357c7bed5
IsisChameleon <12711084+IsisChameleon@users.noreply.github.com>
2024-02-04 08:06:37 +0000

    Not sure about this one

--- decluttr-app/backend/main.py
+++ decluttr-app/backend/main.py
@@ -8,6 +8,10 @@ from app.api.routers.item import item_router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 app = FastAPI()
 
@@ -27,4 +31,4 @@ if environment == "dev":
 app.include_router(chat_router, prefix="/api/chat")
 app.include_router(item_router, prefix="/api/item")
 
-handler = Mangum(app)
+#handler = Mangum(app)
